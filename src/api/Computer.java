37193cc6ccfe04df1056d8880bacf562b2c4f2db
ethanpreble <ethanpreble@umail.ucsb.edu>
2015-04-07 16:16:54 -0700

    changing Task and Computer to be part of the the api package

--- src/api/Computer.java
+++ src/api/Computer.java
@@ -1,4 +1,4 @@
-package compute;
+package api;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
--- src/api/Task.java
+++ src/api/Task.java
@@ -1,4 +1,4 @@
-package compute;
+package api;
 
 public interface Task<T>{
 				T execute();
