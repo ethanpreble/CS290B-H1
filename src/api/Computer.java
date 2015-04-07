eeaeb0db3d8b1c67b9a0d58032b5499b084f9840
ethanpreble <ethanpreble@umail.ucsb.edu>
2015-04-07 16:25:14 -0700

    adding blank files for tasks and adding service name for compute service

--- src/api/Computer.java
+++ src/api/Computer.java
@@ -4,5 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Computer extends Remote {
+				public static String SERVICE_NAME = "ComputeService";
 				<T> T executeTask(Task <T> t) throws RemoteException;
 }
