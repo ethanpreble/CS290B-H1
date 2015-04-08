e4363fee361df06d3173b65033cb88f854b306a5
greg <gp_github@swimr.com>
2015-04-07 19:37:15 -0700

    Filled in ComputerImpl from https://docs.oracle.com/javase/tutorial/rmi/implementing.html

--- src/computer/ComputerImpl.java
+++ src/computer/ComputerImpl.java
@@ -1,9 +1,57 @@
-package computer;
+
+package computer;			// computer = "engine" in tutorial https://docs.oracle.com/javase/tutorial/rmi/implementing.html
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+//import compute.Compute;		// api = "compute"; Computer = "Compute" in tutorial
+import api.Computer;
+//import compute.Task;
+import api.Task;
 
 
-public class ComputerImpl extends java.rmi.remote
-{
+/**
+ * ComputeImpl Class
+ */
+public class ComputeImpl implements Computer {
+
+	/**
+	 * ComputeImpl Constructor
+	 */
+    public ComputeImpl() {
+        super();
+    }
 
 
+    /**
+     * executeTask
+     * @param  Task<T> t
+     * @return   t.execute()
+     */
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
 
+
+    /**
+     * main()
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Computer";
+            Computer computeImpl = new ComputeImpl();
+            Computer stub = (Computer) UnicastRemoteObject.exportObject(computeImpl, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("ComputeImpl bound");
+        } catch (Exception e) {
+            System.err.println("ComputeImpl exception:");
+            e.printStackTrace();
+        }
+    }
 }
