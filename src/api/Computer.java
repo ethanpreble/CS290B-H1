f3ef387aeb99787a9378327a27da01d19cfd0b54
ethanpreble <ethanpreble@gmail.com>
2015-04-09 16:07:13 -0700

    fixing javadoc in Computer

--- src/api/Computer.java
+++ src/api/Computer.java
@@ -1,16 +1,14 @@
 package api;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
 * The Computer interface allows to set up an RMI service that is able to accept class objects that implement the task interface and returns the generic type given.  
 * Classes that implement this interface are expected to be able to accept tasks somehow (ie, via setting up a server or by other means) and then passing these tasks into the execute function.
 * @author  EthanPreble & GregParsons
 * @see https://docs.oracle.com/javase/tutorial/rmi/designing.html
 */
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
 public interface Computer extends Remote {
 
 	public static String SERVICE_NAME = "Computer";
