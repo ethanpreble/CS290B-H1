ab7d986d47705f31c482a3fd8f0cb8c7fb6abbdd
ethanpreble <ethanpreble@gmail.com>
2015-04-09 14:48:24 -0700

    added javadoc comments to Task.java

--- src/api/Task.java
+++ src/api/Task.java
@@ -1,5 +1,12 @@
 package api;
 
+/**
+* The Task interface is intended to allow support for the execution arbitrary of tasks while returning a generic object.  
+* Computing programs can allow for support of running the execute function of classes that implement the Task interface
+* @author  EthanPreble & GregParsons
+* @see https://docs.oracle.com/javase/tutorial/rmi/designing.html
+*/
+
 public interface Task<T>
 {
 	T execute();
